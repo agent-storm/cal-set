e7d6c3847cccefeccfb5f72fe217103ab769f6b1
agent-storm <gujarathisaisrinith@gmail.com>
2023-11-07 22:36:45 +0530

    added options buttons

--- public/javascript/cal-set-main-page.js
+++ public/javascript/cal-set-main-page.js
@@ -1,9 +1,17 @@
 const auth = firebase.auth();
 
 function SignOutPressed() {
-    
     console.log(auth.currentUser);
     auth.signOut();
     window.location = '../index.html';
+}
 
+function OptionChosen(btnId) {
+    console.log("HERE");
+    const Btn = document.getElementById(btnId);
+    if (Btn.style.backgroundColor != "#DD1155"){
+        Btn.style.backgroundColor = "#DD1155";
+    }
+    // console.log(Btn.style.backgroundColor);
+    
 }
