d91991759435c087bd20d3b6cfd8cc98f37a7bfb
agent-storm <gujarathisaisrinith@gmail.com>
2023-11-09 19:01:53 +0530

    button click select added

--- public/javascript/cal-set-main-page.js
+++ public/javascript/cal-set-main-page.js
@@ -1,23 +1,34 @@
 const auth = firebase.auth();
-
 function SignOutPressed() {
     console.log(auth.currentUser);
     auth.signOut();
     window.location = '../index.html';
 }
-
-function OptionChosen(btnId) {
-    console.log("HERE");
-    const Btn = document.getElementById(btnId);
-    var style = getComputedStyle(Btn);
-    JsonReader();
-    // const obj = JSON.parse(JsonReader());
-    // console.log(obj);
+const OptionStatus = {
+    "timeframe" : {
+        "one-week-btn":0,
+        "one-month-btn":0
+    },
+    "platform" : {
+        "codechef-btn":0,
+        "codeforces-btn":0,
+        "hackerrank-btn":0,
+        "gfg-btn":0
+    },
+    "preset" : {
+        "codeforces-preset-btn":0,
+        "codechef1w-preset-btn":0,
+        "codechef-full-preset-btn":0
+    }
 }
-function JsonReader() { 
-    fetch("../json/options.json") 
-        .then((res) => { 
-        console.log(res.json());
-        // return res.json(); 
-    });
+function OptionChosen(section,btnId) {
+    const btn = document.getElementById(btnId);
+    if(OptionStatus[section][btnId] == 0){
+        OptionStatus[section][btnId] = 1;
+        btn.style.backgroundColor = "#DD1155";
+    }
+    else {
+        OptionStatus[section][btnId] = 0;
+        btn.style.backgroundColor = "#A9ACA9";
+    }
 }
