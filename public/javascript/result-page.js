bc504ac1bbe12574731d5afd3d067f29e23f06cb
agent-storm <gujarathisaisrinith@gmail.com>
2024-01-24 03:36:30 +0530

    GAPI test

--- public/javascript/result-page.js
+++ public/javascript/result-page.js
@@ -17,7 +17,7 @@ Object.values(contests).forEach((contest)=> {
     let contest_link = contest["href"];
     data.push([platform_name,contest_name,contest_time.split("@")[0],contest_time.split("@")[1],contest_duration,contest_link]);
 });
-
+console.log(data);
 let ul = document.getElementById("contest-list");
 ul.innerHTML = all_list_items;
 
@@ -89,50 +89,39 @@ function handleAuthClick() {
 
 async function CreateEvents() {
     console.log("EventCreator");
-    const event = {
-        'summary': 'Google I/O 2015',
-        'location': '800 Howard St., San Francisco, CA 94103',
-        'description': 'A chance to hear more about Google\'s developer products.',
-        'start': {
-          'dateTime': '2015-05-28T09:00:00-07:00',
-          'timeZone': 'America/Los_Angeles'
-        },
-        'end': {
-          'dateTime': '2015-05-28T17:00:00-07:00',
-          'timeZone': 'America/Los_Angeles'
-        },
-        'recurrence': [
-          'RRULE:FREQ=DAILY;COUNT=2'
-        ],
-        'attendees': [
-          {'email': 'lpage@example.com'},
-          {'email': 'sbrin@example.com'}
-        ],
-        'reminders': {
-          'useDefault': false,
-          'overrides': [
-            {'method': 'email', 'minutes': 24 * 60},
-            {'method': 'popup', 'minutes': 10}
-          ]
-        }
-      };
-      
-      const request = gapi.client.calendar.events.insert({
-        'calendarId': 'primary',
-        'resource': event
-      });
-      
-      request.execute(function(event) {
-        console.log('Event created: ' + event.htmlLink);
-      });
+    data.slice(1,).forEach((contest)=>{
+        const event = {
+            'summary': `${contest}`,
+            'location': '',
+            'description': 'This is to check if events can be added to clients Gmail.',
+            'start': {
+              'dateTime': '2024-01-25T00:00:00+05:30', // Adjusted for Indian Timezone (IST)
+              'timeZone': 'Asia/Kolkata'
+            },
+            'end': {
+              'dateTime': '2024-01-26T00:00:00+05:30', // Adjusted for Indian Timezone (IST)
+              'timeZone': 'Asia/Kolkata'
+            },
+            'recurrence': [
+              'RRULE:FREQ=DAILY;COUNT=1'
+            ],
+            'reminders': {
+              'useDefault': false,
+              'overrides': [
+                {'method': 'email', 'minutes': 24 * 60},
+                {'method': 'popup', 'minutes': 10}
+              ]
+            }
+          };
+          
+          const request = gapi.client.calendar.events.insert({
+            'calendarId': 'primary',
+            'resource': event
+          });
+          
+          request.execute(function(event) {
+            console.log('Event created: ' + event.htmlLink);
+          });
+    });
 }
 
-// async function AddToCalendar() {
-//     console.log(gapi.auth2.getAuthInstance());
-//     if(gapiInited && gisInited) {
-//         gapi.auth2.getAuthInstance().signIn().then(function () {
-//             console.log('User signed in.');
-//             // You can now make API requests.
-//           });
-//     }
-// }
